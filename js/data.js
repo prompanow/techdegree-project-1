e125f2dbe5ba332b738fbe7649a406e6d94c4093
prompanow <cbps@Caspers-MBP.lan>
2018-02-17 10:47:05 +0100

    Final update

---
+++ js/data.js
@@ -0,0 +1,58 @@
+// This file contains all of the quotes in a array of object
+
+var quotes = [
+  {
+    quote: 'Every child is an artist, the problem is staying an artist when you grow up',
+    source: 'Pablo Picasso',
+    citation: '',
+    year: ''
+  },
+  {
+    quote: 'If you hear a voice within you say, you cannot paint, then by all means paint, and that voice will be silenced',
+    source: 'Vincent Van Gogh',
+    citation: '',
+    year: ''
+  },
+  {
+    quote: 'Have no fear of perfection, you will never reach it',
+    source: 'Salvador Dali',
+    citation: '',
+    year: ''
+  },
+  {
+    quote: 'You miss 100% of the shots you don’t take',
+    source: 'Wayne Gretzky',
+    citation: '',
+    year: ''
+  },
+  {
+    quote: 'The most difficult thing is the decision to act, the rest is merely tenacity',
+    source: 'Amelia Earhart',
+    citation: '',
+    year: ''
+  },
+  {
+    quote: 'Every strike brings me closer to the next home run',
+    source: 'Babe Ruth',
+    citation: '',
+    year: ''
+  },
+  {
+    quote: 'Life is not about getting and having, it is about giving and being',
+    source: 'Kevin Kruse',
+    citation: '',
+    year: ''
+  },
+  {
+    quote: 'We become what we think about',
+    source: 'Earl Nightingale',
+    citation: '',
+    year: ''
+  },
+  {
+    quote: 'Life is 10% what happens to me and 90% of how I react to it',
+    source: 'Charles Swindoll',
+    citation: '',
+    year: ''
+  }
+];
--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
+// This function randomly selects an object in an given array
+function getRandomQuote (quote) {
+  return quote[Math.floor(Math.random() * Math.floor(quote.length))];
+};
+
+// This function calls the getRandomQuote function and creates a html-layout that will display a random quote
+function printQuote (quote) {
+  var randomQuote = getRandomQuote(quote);
+  var quoteHtml = '<p class="quote">' + randomQuote.quote + '</p>';
+  quoteHtml += '<p class="source">' + randomQuote.source;
+  quoteHtml += '<span class="citation">' + randomQuote.citation + '</span>';
+  quoteHtml += '<span class="year">' + randomQuote.year + '</span>';
+  quoteHtml += '</p>';
+  document.getElementById('quote-box').innerHTML = quoteHtml;
+};
+
+printQuote(quotes);
+
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
+document.getElementById('loadQuote').addEventListener("click", function() { printQuote(quotes);
+    }, false);
